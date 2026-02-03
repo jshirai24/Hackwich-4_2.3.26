0617ef96a3f2ed5fb4c779cea56dfdc738cf13bd
Jillian Shirai <jtshirai@hawaii.edu>
2026-02-03 12:21:57 -1000

    Completed Hackwich 4 In class portion

---
+++ Hackwich 4_2.3.26/Second ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Second ViewController.swift
+//  Hackwich 4_2.3.26
+//
+//  Created by Jillian Shirai on 2/3/26.
+//
+
+import UIKit
+
+class Second_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
