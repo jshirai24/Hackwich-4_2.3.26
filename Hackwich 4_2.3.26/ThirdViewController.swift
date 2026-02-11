f4968383cea596d5834f2ae3d773d47a553cad15
Jillian Shirai <jtshirai@hawaii.edu>
2026-02-10 17:57:06 -1000

    Added the class and to do tabs, need to figure out if we need to change the tab text programmatically

---
+++ Hackwich 4_2.3.26/ThirdViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdViewController.swift
+//  Hackwich 4_2.3.26
+//
+//  Created by Jillian Shirai on 2/10/26.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
